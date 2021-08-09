d09fbcab13b85b770c4f90f6e26f2cf0b1789bc1
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2021-08-09 09:42:10 -0700

    Append query and search results to output file
    Append query and search results to output file

--- google_search.py
+++ google_search.py
@@ -4,7 +4,15 @@ except ImportError:
 	print("No module named 'google' found")
 
 # to search
-query = "A computer science portal"
+query = "active volcano"
 
-for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-	print(j)
+#store query and search results in output.txt file
+output_file = open("output.txt", a)
+print("***********************", file=output_file)
+print(query, file=output_file)
+print("***********************", file=output_file)
+
+for i in search(query, tld="com", lang='en', num=10, stop=10, pause=2):
+	print(i, file=output_file)
+print("***********************", file=output_file)
+f.close()
