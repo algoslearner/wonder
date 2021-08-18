a8b044dc087cd0f9816e42786ba532fa0b24972d
roshini-michael <roshini.michael09@gmail.com>
2021-08-17 17:48:46 -0700

    Using custom search API url

--- search_customAPI.py
+++ search_customAPI.py
@@ -1,20 +1,26 @@
 import requests
-import urllib.parse as urlparse
 
+# get the API KEY here: https://developers.google.com/custom-search/v1/overview
+API_KEY = "AIzaSyBe_rpbTPsQrB_dmTCuSCpIBN8jc0ar-P0"
+
+# get your Search Engine ID on your CSE control panel
+SEARCH_ENGINE_ID = "3b0a0eb2b684072d2"
+
+# the search query you want
 query = input("Enter your Google search query :" )
-params = {
-	"key": "AIzaSyBe_rpbTPsQrB_dmTCuSCpIBN8jc0ar-P0",
-	"cx": "3b0a0eb2b684072d2",
-	"q": query,
-	"start": 0
-}
-gcse_base_url = "https://www.googleapis.com/customsearch/v1"
-url_parts = list(urlparse.urlparse(gcse_base_url))
-url_parts[4] = urlparse.urlencode(params)
-gcse_url = urlparse.urlunparse(url_parts)
 
-data = requests.get(gcse_url).json()
-items = data["items"]
+# using the first page
+page = 1
+
+# constructing the URL
+# doc: https://developers.google.com/custom-search/v1/using_rest
+# calculating start, (page=2) => (start=11), (page=3) => (start=21)
+start = (page - 1) * 10 + 1
+url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
+
+# make the API request
+# using requests' json() method to automatically parse the returned JSON data to a Python dictionary
+data = requests.get(url).json()
 
 # output json data to textfile
 output_file = open("outputJSON.txt", 'w')
